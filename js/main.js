4655f51fa9241d1f4c7c00a404aec98a11e924bf
tobiashungwe <tobias.hungwe@student.howest.be>
2022-09-16 20:48:34 +0200

    Update css styling for mobile

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("Sep 27, 2022 00:00:00").getTime(); 
+
+//globale variables
+
+var x = setInterval(function(){
+
+    var counter = document.getElementById("launch");
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    counter.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "; 
+
+    if(distance < 0) {
+        clearInterval(x);
+        counter.innerHTML = "EXPIRED"; 
+    }
+    
+}, 1000); 
