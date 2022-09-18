db104218fd22d95bf65ea18dc8ff34c44adfd473
Tobias Hungwe <tobias.hungwe@student.howest.be>
2022-09-18 02:38:42 +0200

    Add navbar with scss

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <h1>App</h1>
-  );
-}
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'; 
+
+import { images } from '../../constants'; 
+import './Navbar.scss';
+const Navbar = () => {
+  return (
+    
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['home','about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex" key={`link-${item}`}>
+
+            <div></div>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+    
+
+  ); 
+}
+
+export default Navbar;
