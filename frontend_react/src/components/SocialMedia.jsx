7bf0d73f5cff0bf4dc5fbaa74c607c6d9f29b799
tobiashungwe <tobias.hungwe@student.howest.be>
2023-08-26 19:55:50 +0200

    add github page to social media buttons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BsTwitter, BsInstagram} from  'react-icons/bs'
+import {BsTwitter, BsInstagram, BsGithub} from  'react-icons/bs'
 import {FaFacebookF} from 'react-icons/fa';
 
 
@@ -7,11 +7,15 @@ import {FaFacebookF} from 'react-icons/fa';
 const SocialMedia = () => {
   return (
     <div className="app__social">
+       
         <div>
           <a href="https://twitter.com/HungweVision"> <BsTwitter/></a>
         </div>
         <div>
             <a href="https://www.facebook.com/tobiashungwe"><FaFacebookF/></a>
+        </div>
+        <div>
+          <a href="https://github.com/tobiashungwe"> <BsGithub/></a>
         </div>
         <div>
             <a href="https://www.instagram.com/hungwevision/"><BsInstagram/></a>
