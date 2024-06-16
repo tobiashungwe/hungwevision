dc3c7cb760c83af1af070a02328ecf93c84206ed
tobiashungwe <tobias.hungwe@student.howest.be>
2024-06-16 13:54:05 +0200

    refactor: Export images object in images.js
    The images.js file was refactored to export the images object using the `const` keyword instead of the `export default` statement. This change improves the code organization and follows the established convention of using `const` for exporting objects.

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -37,7 +37,7 @@ import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 
-export default {
+const images = {
   email,
   mobile,
   dotnet,
@@ -73,3 +73,5 @@ export default {
   skype,
   spotify,
 };
+
+export default images;
